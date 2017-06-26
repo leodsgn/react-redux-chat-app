fecf1a1fe008b955fb102e173b1770154b0a68e6
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2017-06-26 14:28:58 -0300

    procfile and server fixed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: yarn start && node ./server/index.js
+web: node ./server/index.js
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const path = require('path');
 
+app.use(express.static(path.resolve(__dirname, "../build")));
+
 app.get("/", function(req, res){
-    res.sendFile(path.resolve(__dirname, "../public/index.html"));
+    res.sendFile(path.resolve(__dirname, "../build/index.html"));
 }) 
 
 http.listen(3001, function(){
